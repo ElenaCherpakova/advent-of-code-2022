b7c36c9c225bd24e27895b29cb8c1889d5ad9a46
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-12-04 19:28:59 -0500

    refactor code for day3

--- day03.js
+++ day03.js
@@ -1,67 +1,47 @@
-// const sampleInput = `vJrwpWtwJgWrhcsFMMfFFhFp
-// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-// PmmdzqPrVvPwwTWBwg
-// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-// ttgJtRGJQctTZtZT
-// CrZsJsPPZsGzwwsLwLmpwMDw
-// `;
+const sampleInput = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`;
 
-const fetchInput = async (day) => {
-  const response = await fetch(
-    `https://adventofcode.com/2022/day/${day}/input`,
-  );
-  const text = await response.text();
-  return text.trim();
-};
-const sampleInput = await fetchInput(3);
 
-const format = (input) => {
-  return input.trim().split('\n');
-};
+const rucksacks = sampleInput.split('\n');
 
-const letterToNumber = (letter) => {
-  if (/[a-z]/.test(letter)) {
-    return letter.charCodeAt(0) - 96;
-  }
-  if (/[A-Z]/.test(letter)) {
-    return letter.charCodeAt(0) - 65 + 27;
-  }
+const findCommonItems = (rucksacks) => {
+  const halfIndex = rucksacks.length / 2;
+  const [firstHalf, secondHalf] = [
+    rucksacks.slice(0, halfIndex),
+    rucksacks.slice(halfIndex),
+  ];
+  const firstHalfSet = new Set(firstHalf);
+  const commonItem = [...secondHalf].find((item) => firstHalfSet.has(item));
+  return commonItem;
 };
 
-const part1 = () => {
-  const inputFormatted = format(sampleInput);
-  const res = inputFormatted.map((line) => {
-    const part1 = [...line.slice(0, line.length / 2)];
-    const part2 = [...line.slice(line.length / 2)];
+const letterToNumber = (letter) =>
+  letter.charCodeAt() - (/[a-z]/.test(letter) ? 96 : 38);
+const sum = rucksacks
+  .map((item) => findCommonItems(item))
+  .map((item) => letterToNumber(item))
+  .reduce((a, b) => a + b, 0);
 
-    let part1Set = new Set(part1);
-    const intersection = part2.filter((x) => part1Set.has(x));
-    let removeDuplicates = [...new Set(intersection)];
+console.log(sum);
 
-    return letterToNumber(removeDuplicates[0]);
-  });
-  return res.reduce((a, b) => a + b, 0);
+// Part 2
+
+const getGroupOf3 = (arr) =>
+  arr.length ? [arr.slice(0, 3), ...getGroupOf3(arr.slice(3))] : [];
+
+const findCommonItemInGroup3 = ([sack1, sack2, sack3]) => {
+  const [set1, set2, set3] = [new Set(sack1), new Set(sack2), new Set(sack3)];
+  const commonItem = [...set3].find((item) => set1.has(item) && set2.has(item));
+  return commonItem;
 };
+//convert to common letter To Numbers and sum them up
+const sumTotal = getGroupOf3(rucksacks)
+  .map((item) => findCommonItemInGroup3(item))
+  .map((item) => letterToNumber(item))
+  .reduce((acc, num) => acc + num, 0);
 
-console.log(part1(sampleInput));
-
-const part2 = () => {
-  let sum = 0;
-  const formatted = format(sampleInput);
-  for (let i = 0; i < formatted.length; i += 3) {
-    const backpacks = [formatted[i], formatted[i + 1], formatted[i + 2]];
-    // removing duplicated letters from the first backpack
-    let firstUniqueGroup = new Set(backpacks[0]);
-    // find same letter in the second backpack
-    let intersection = [...backpacks[1]].filter((x) => firstUniqueGroup.has(x));
-    // remove duplicates from the intersection
-    firstUniqueGroup = new Set(intersection);
-    // find same letter in the third backpack
-    intersection = [...backpacks[2]].filter((x) => firstUniqueGroup.has(x));
-    let removeDuplicates = [...new Set(intersection)];
-    sum += letterToNumber(removeDuplicates[0]);
-  }
-  return sum;
-};
-
-console.log(part2(sampleInput));
+console.log(sumTotal);
